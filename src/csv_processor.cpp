8d012ba5e8146cb7e0c11bb33428b14b359c2efd
SHoradagoda <sid.horadagoda@gmail.com>
2017-06-19 05:05:54 +1000

    Initial Draft

---
+++ src/csv_processor.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Siddath Horadagoda on 19/06/2017.
+//
+
+#include "csv_processor.h"
+
+int strip_columns ( string file_name ) {
+
+
+}
---
+++ src/csv_processor.h
@@ -0,0 +1,12 @@
+//
+// Created by Siddath Horadagoda on 19/06/2017.
+//
+
+#ifndef GTEST_DEMO_CSV_PROCESSOR_H
+#define GTEST_DEMO_CSV_PROCESSOR_H
+
+using namespace std;
+
+int strip_columns ( string file_name );
+
+#endif //GTEST_DEMO_CSV_PROCESSOR_H
